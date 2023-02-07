d1450b5ac0dbc556a113436c231a123020e5a7ed
cenfun <cenfun@gmail.com>
2023-02-07 18:48:43 +0800

    update stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,15 +1,3 @@
 module.exports = {
-    extends: ["stylelint-config-recommended", "stylelint-config-standard"],
-    overrides: [{
-        customSyntax: "postcss-scss",
-        files: ["**/*.scss"]
-    }, {
-        customSyntax: "postcss-html",
-        files: ["**/*.html", "**/*.vue"]
-    }],
-    rules: {
-        "at-rule-no-unknown": null,
-        "font-family-no-missing-generic-family-keyword": null,
-        "indentation": 4
-    }
+    extends: ["stylelint-config-plus"]
 };
