cefdeec19807d9e680660f50da72952cb3c81cb5
cenfun <cenfun@gmail.com>
2023-04-18 20:22:31 +0800

    update cli show-report

--- packages/vendor/src/index.js
+++ packages/vendor/src/index.js
@@ -1,15 +1,21 @@
-import nodemailer from 'nodemailer';
 import StackUtils from 'stack-utils';
 import { codeFrameColumns } from '@babel/code-frame';
+
 import { parse } from '@babel/parser';
+
+import nodemailer from 'nodemailer';
+
 import Koa from 'koa';
 import open from 'open';
 
 export {
-    nodemailer,
     StackUtils,
     codeFrameColumns,
+
     parse,
+
+    nodemailer,
+
     Koa,
     open
 };
