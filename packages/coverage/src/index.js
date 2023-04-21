81b85bdcd87622e6f5461654a48492791843eb56
cenfun <cenfun@gmail.com>
2023-04-22 00:28:28 +0800

    update coverage

--- packages/coverage/src/index.js
+++ packages/coverage/src/index.js
@@ -2,9 +2,11 @@
 import V8toIstanbul from 'v8-to-istanbul';
 import istanbulLibCoverage from 'istanbul-lib-coverage';
 import istanbulLibReport from 'istanbul-lib-report';
+import { mapFileCommentRegex } from 'convert-source-map';
 
 export {
     V8toIstanbul,
     istanbulLibCoverage,
-    istanbulLibReport
+    istanbulLibReport,
+    mapFileCommentRegex
 };
