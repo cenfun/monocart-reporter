eda0faae1e77f4ddbf403c91ce8d937ed5e58dc2
cenfun <cenfun@gmail.com>
2023-06-16 19:13:29 +0800

    update types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -35,23 +35,38 @@ export function addCoverageReport(
 export type CoverageReportOptions = {
 
     title?: string,
-    // outputDir?: string,
+    outputDir?: string,
     outputName?: string,
 
+    // Whether to convert to Istanbul report
     toIstanbul?: boolean,
 
+    // A filter function to execute for each element in the V8 list.
     entryFilter?: (entry: any) => boolean,
 
+    // Whether to unpack all sources from the source map if a related source map file is found.
     unpackSourceMap?: boolean,
+    // Whether to exclude the dist file (usually minified) if the sources are successfully unpacked from the source map.
     excludeDistFile?: boolean,
+
+    // A filter function to execute for each element in the sources which unpacked from the source map.
     sourceFilter?: (sourcePath: string) => boolean,
 
     sourcePath?: (sourcePath: string) => string,
 
+    sourceFinder?: (filePath: string) => string,
+
+    // Whether to create `lcov.info`
     lcov?: boolean,
 
-    watermarks?: number[] | any,
+    watermarks?: [number, number] | {
+        statements: [number, number],
+        functions: [number, number],
+        branches: [number, number],
+        lines: [number, number]
+    },
 
+    // Whether inline all scripts to the single HTML file.
     inline?: boolean
 
 };
@@ -67,8 +82,17 @@ export function attachCoverageReport(
 /**
  * network
  */
+export type NetworkReportOptions = {
+    title?: string,
+    outputDir?: string,
+    outputName?: string,
+
+    // Whether inline all scripts to the single HTML file.
+    inline?: boolean
+};
+
 export function attachNetworkReport(
     har: string | Buffer,
     testInfo: TestInfo,
-    options?: any
+    options?: NetworkReportOptions
 ): Promise<any>;
