6caa730efff63f868ec35c091e5929f67cf8e65b
cenfun <cenfun@gmail.com>
2023-06-30 13:04:49 +0800

    fix trace UI

--- packages/reporter/src/components/detail/attachments/trace.vue
+++ packages/reporter/src/components/detail/attachments/trace.vue
@@ -1,9 +1,12 @@
 <script setup>
 import { onMounted, shallowReactive } from 'vue';
+import { components } from 'vine-ui';
 
 import Util from '../../../utils/util.js';
 import state from '../../../modules/state.js';
 import AttachmentHead from './attachment-head.vue';
+
+const { VuiFlex } = components;
 
 const props = defineProps({
     data: {
@@ -50,69 +53,69 @@ onMounted(() => {
       >{{ props.data.name }}</a>
     </AttachmentHead>
     <div class="mcr-attachment-body">
-      <div>
-        <a
-          :href="d.viewerUrl"
-          target="_blank"
-        >View trace</a>
-      </div>
-      <details>
-        <summary>NOTE</summary>
+      <VuiFlex
+        padding="10px"
+        gap="10px"
+        align-items="baseline"
+      >
+        <div>
+          <a
+            :href="d.viewerUrl"
+            target="_blank"
+          >View trace</a>
+        </div>
+        <details class="vui-flex-auto">
+          <summary>
+            NOTE
+          </summary>
 
-        <dl class="mcr-readme">
-          <dd class="mcr-item">
-            The <a
-              href="https://trace.playwright.dev/"
-              target="_blank"
-            >Trace Viewer</a> requires that the trace file must be loaded over the http:// or https:// protocols (current protocol is <code :style="d.color">{{ d.protocol }}</code>)
-            without <a
-              href="https://developer.mozilla.org/en-US/docs/Glossary/CORS"
-              target="_blank"
-            >CORS</a> issue,
-            try <code>npx monocart show-report &lt;your-outputFile-path&gt;</code> start a local web server, please keep attachments and reports under the same directory.
-          </dd>
-          <dd class="mcr-item">
-            or download the trace file and load it to the page <a
-              href="https://trace.playwright.dev/"
-              target="_blank"
-            >Trace Viewer</a> manually.
-          </dd>
-          <dd class="mcr-item">
-            or customize a trace viewer url with option <code>traceViewerUrl: "{{ defaultUrl }}"</code>
-          </dd>
-        </dl>
-      </details>
+          <dl class="mcr-readme">
+            <dd class="mcr-item">
+              The <a
+                href="https://trace.playwright.dev/"
+                target="_blank"
+              >Trace Viewer</a> requires that the trace file must be loaded over the http:// or https:// protocols (current protocol is <code :style="d.color">{{ d.protocol }}</code>)
+              without <a
+                href="https://developer.mozilla.org/en-US/docs/Glossary/CORS"
+                target="_blank"
+              >CORS</a> issue,
+              try <code>npx monocart show-report &lt;your-outputFile-path&gt;</code> start a local web server, please keep attachments and reports under the same directory.
+            </dd>
+            <dd class="mcr-item">
+              or download the trace file and load it to the page <a
+                href="https://trace.playwright.dev/"
+                target="_blank"
+              >Trace Viewer</a> manually.
+            </dd>
+            <dd class="mcr-item">
+              or customize a trace viewer url with option <code>traceViewerUrl: "{{ defaultUrl }}"</code>
+            </dd>
+          </dl>
+        </details>
+      </VuiFlex>
     </div>
   </details>
 </template>
 
 <style lang="scss">
 .mcr-attachment-trace {
-    .mcr-attachment-body {
-        display: flex;
-        flex-direction: row;
-        padding: 10px;
+    a {
+        white-space: nowrap;
+    }
 
-        a {
-            white-space: nowrap;
+    details {
+        summary {
+            color: #666;
+            cursor: pointer;
+            user-select: none;
         }
 
-        details {
-            summary {
-                margin-left: 10px;
-                color: #666;
-                cursor: pointer;
-                user-select: none;
-            }
+        dl {
+            margin-top: 5px;
+        }
 
-            dl {
-                margin-top: 5px;
-                margin-left: 10px;
-            }
-
-            dd {
-                margin: 0;
-            }
+        dd {
+            margin: 0;
         }
     }
 }
