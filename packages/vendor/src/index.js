9268eab24d72e6c0b872f9d9c8dafee9f567c1a8
cenfun <cenfun@gmail.com>
2024-01-13 11:27:42 +0800

    support ssl for monocart cli

--- packages/vendor/src/index.js
+++ packages/vendor/src/index.js
@@ -2,11 +2,13 @@ import StackUtils from 'stack-utils';
 import { codeFrameColumns } from '@babel/code-frame';
 import WebSocket, { WebSocketServer } from 'ws';
 import sanitize from 'sanitize-filename';
+import { program } from 'commander';
 
 export {
     StackUtils,
     codeFrameColumns,
     WebSocket,
     WebSocketServer,
-    sanitize
+    sanitize,
+    program
 };
