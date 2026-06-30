03372f8ee329ff79c38bb75fe74d6df189528482
Jack5 <32806494+jack5github@users.noreply.github.com>
2026-06-30 16:03:01 +1000

    Style and word wrap test metadata

--- packages/app/src/components/detail/detail-simple-list.vue
+++ packages/app/src/components/detail/detail-simple-list.vue
@@ -28,11 +28,24 @@ const props = defineProps({
 
 <style lang="scss">
 .mcr-simple-column {
+    align-items: flex-start;
     min-width: 20px;
+    max-width: 100%;
+    margin-left: auto;
     padding: 2px 6px;
     font-size: 13px;
     border: 1px solid #ddd;
     border-radius: 5px;
     background-color: rgb(175 184 193 / 20%);
+
+    > div:first-child {
+        font-weight: bold;
+    }
+
+    > div:last-child {
+        flex-shrink: 1;
+        white-space: normal;
+        overflow-wrap: anywhere;
+    }
 }
 </style>
